2a7d04800ea4c23d86f8be6b7f1a99534199eb35
ryoki-0703 <tacchimi504@gmail.com>
2021-06-09 04:47:33 +0000

    figuresコントローラー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_093834) do
+ActiveRecord::Schema.define(version: 2021_06_09_042724) do
+
+  create_table "figures", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
