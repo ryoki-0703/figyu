18358fd0200a10c5b7e1be5472482987c9036690
ryoki-0703 <tacchimi504@gmail.com>
2021-06-14 12:25:54 +0000

    figureコントローラー修正

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,2 +1,16 @@
 class FiguresController < ApplicationController
+  def new
+    @figure = Figure.new
+  end
+
+  def create
+    figure = Figure.new(figure_params)
+    figure.save
+    redirect_to "root_path"
+  end
+
+  private
+  def figure_params
+    params.require(:figure).permit(:title, :body)
+  end
 end
